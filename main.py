48482aed380ec622f81cff15e60f4aed45db144f
Arthur <elyjax@riseup.net>
2016-10-28 00:20:16 +0200

    Passage au francais

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
-def hideText(fileName, text):
-    fi = open(fileName, 'rb')
+def cacherTexte(nomFichier, texte):
+    fi = open(nomFichier, 'rb')
     fo = open('out', 'wb')
 
     fo.write(fi.read(54))
-    for c in text + chr(4):
-        letterCode = bin(ord(c))[2:].zfill(8)
+    for c in texte + chr(4):
+        codeLettre = bin(ord(c))[2:].zfill(8)
         for i in range(0, 4):
-            pixelCode = bin(int.from_bytes(fi.read(1), 'big'))[2:].zfill(8)
-            newCode = pixelCode[0:6] + letterCode[i*2:(i*2)+2]
-            fo.write(int(newCode, 2).to_bytes(1, 'big'))
+            codePixel = bin(int.from_bytes(fi.read(1), 'big'))[2:].zfill(8)
+            nouveauCode = codePixel[0:6] + codeLettre[i*2:(i*2)+2]
+            fo.write(int(nouveauCode, 2).to_bytes(1, 'big'))
     fo.write(fi.read())
     fo.close()
     fi.close()
 
-def extractText(fileName):
-    fi = open(fileName, 'rb')
+def extraireTexte(nomFichier):
+    fi = open(nomFichier, 'rb')
     fi.seek(54)
-    text = ""
+    texte = ""
     EOF = False
     while not EOF:
-        letterCode = ""
+        codeLettre = ""
         for i in range(0, 4):
-            pixelCode = bin(int.from_bytes(fi.read(1), 'big'))[2:].zfill(8)
-            letterCode += pixelCode[6:]
-        if letterCode == '00000100':
+            codePixel = bin(int.from_bytes(fi.read(1), 'big'))[2:].zfill(8)
+            codeLettre += codePixel[6:]
+        if codeLettre == '00000100':
             EOF = True
         else:
-            text += chr(int(letterCode, 2))
+            texte += chr(int(codeLettre, 2))
     fi.close()
-    return text
+    return texte
 
-def imageSwap(fileName):
-    fi = open(fileName, 'rb')
-    fo = open(fileName + ".swp", 'wb')
+def inverserImage(nomFichier):
+    fi = open(nomFichier, 'rb')
+    fo = open(nomFichier + ".swp", 'wb')
 
     fo.write(fi.read(54))
     for c in fi.read():
-        pixelCode = bin(c)[2:].zfill(8)
-        newCode = pixelCode[4:8] + pixelCode[0:4]
-        fo.write(int(newCode, 2).to_bytes(1, 'big'))
+        codePixel = bin(c)[2:].zfill(8)
+        nouveauCode = codePixel[4:8] + codePixel[0:4]
+        fo.write(int(nouveauCode, 2).to_bytes(1, 'big'))
     fo.write(fi.read())
     fo.close()
     fi.close()
-
-hideText("img.bmp", "hello!")
-imageSwap("img.bmp")
-imageSwap("img.bmp.swp")

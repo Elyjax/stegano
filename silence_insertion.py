8bcdf707d78c2267be1bc84beddab35fc9fd0f0a
Arthur <elyjax@riseup.net>
2016-11-20 00:09:02 +0100

    Première version fonctionnelle de la méthode des silences
    Ajout d'une fonction duree_silence
    Reformatage des espaces pour respecter les conventions Python

--- silence_insertion.py
+++ silence_insertion.py
@@ -15,95 +15,80 @@ import numpy as np
 
 #variable globale
 dtypemode = 'int16'
+bits_nb_octets = 32 # Nombre de bits utilisés pour stocker la taille du message
 
-def est_silence(t,indice) :
-    ndtype=int(dtypemode[3:])
-    return abs(t[indice]/(2**ndtype)) < 1/100
+def est_silence(t, indice):
+    ndtype = int(dtypemode[3:])
+    return abs(t[indice] / (2**ndtype)) < 1/100
 
-def len_min_silences(nbitcodage) : 
-    n=1
-    while (2**nbitcodage-1)/n > 0.1 :
-        n=n+1
+def len_min_silences(nbitcodage):
+    n = 1
+    while (2**nbitcodage-1) / n > 0.1 :
+        n += 1
     return n
 
-#coder_ds_wav('discours1(1.1s).wav',txt,1,'int16')
+def duree_silence(t, i):
+    k = i + 1
+    while est_silence(t, k) and k < len(t):
+        k += 1
+    return k - i
 
-def modification_silence(t,i,morceauchaine,nbitcodage):
-    n = len(t)
-    longueursilence=1
-    k=i+1
-    while est_silence(t,k) and k < n :
-        k=k+1
-        longueursilence = longueursilence + 1
-    x = int(morceauchaine,2)
-    beta = n % (2**nbitcodage)
-    ajoutlongueur = x-beta
-    if ajoutlongueur == 0 or len(morceauchaine) == 0 :
-        return (False,i+n)
-    elif ajoutlongueur < 0 :
-        ajoutlongueur = ajoutlongueur + (2**nbitcodage)
-        for j in range(0,ajoutlongueur) :
-            t.insert(i+n+j,0)
-        return(True,i+n+ajoutlongueur)
-    else :
-        for j in range(0,ajoutlongueur) :
-            t.insert(i+n+j,0)
-        return(True,i+n+ajoutlongueur)
+def modification_silence(t, i, valeur_a_cacher, nbitcodage):
+    longueur_silence = duree_silence(t, i)
+    valeur_a_cacher = int(valeur_a_cacher, 2)
+    ajout_silences = valeur_a_cacher - (longueur_silence % (2**nbitcodage))
+    while ajout_silences < 0:
+        ajout_silences += (2**nbitcodage)
+    for j in range(ajout_silences):
+        t.insert(i, 0)
+    return (True, i + longueur_silence + ajout_silences)
 
-def cacher_dans_silences(nomfichier,message,nbitcodage):
-    f = wavfile.read(nomfichier)
+def cacher_dans_silences(nom_fichier, message, nbitcodage):
+    f = wavfile.read(nom_fichier)
     t = f[1] #array complet du son
     t = list(t)
-    lenmessage = len(message)
     n = len(t)
-    nombre_octets = np.binary_repr(n,30) #chaine binaire de la longueur du txt sur 30 bit comptant les octets
-    chaine = nombre_octets + message
+    # Nombre d'octets du message à cacher représenté sur bits_nb_octets
+    nb_octets = np.binary_repr(len(message) // 8, bits_nb_octets)
+    message = nb_octets + message
     i = 0
-    imessage = 0
-    while i < n :
-        if est_silence(t,i) :
-            (changement,i) = modification_silence(t,i,chaine[imessage:imessage+nbitcodage],nbitcodage)
-            if changement :
-                imessage=imessage+nbitcodage
-        else :
-            i=i+1
-    t = np.asarray(t,dtype=np.int16)
-    wavfile.write("resultat.wav",44100,t)
-    
-def extraire_depuis_silences(nomfichier,nbitcodage):
-    f = wavfile.read(nomfichier)
+    i_message = 0
+    while i < n and i_message < len(message):
+        if est_silence(t, i):
+            valeur_a_cacher = message[i_message:i_message+nbitcodage]
+            (changement, i) = modification_silence(t, i, valeur_a_cacher, nbitcodage)
+            if changement:
+                i_message += nbitcodage
+        else:
+            i += 1
+    t = np.asarray(t, dtype=np.int16)
+    wavfile.write("resultat.wav", 44100, t)
+
+def extraire_depuis_silences(nom_fichier, nbitcodage):
+    f = wavfile.read(nom_fichier)
     t = f[1]
-    message = ''
+    message = ""
     n = len(t)
     i = 0
-    while len(message) < 30 :
-        if est_silence(t,i) :
-            longueursilence=1
-            k=i+1
-            while k < n and est_silence(t,k) :
-                k=k+1
-                longueursilence = longueursilence + 1
-            valeur = longueursilence % (2**nbitcodage)
-            message = message + np.binary_repr(valeur,2**nbitcodage)
-            i = i + longueursilence
+    while len(message) < bits_nb_octets:
+        if est_silence(t, i):
+            longueur_silence = duree_silence(t, i)
+            valeur = longueur_silence % (2**nbitcodage)
+            message += np.binary_repr(valeur, nbitcodage)
+            i += longueur_silence
         else :
-            i=i+1
-    longueurmessage = int(message[0:30],2)*8
-    j = 0
-    while j < n and len(message) < (30 + longueurmessage) :
-        if est_silence(t,j) :
-            longueursilence=1
-            k=j+1
-            while k < n and est_silence(t,k):
-                k=k+1
-                longueursilence = longueursilence + 1
-            valeur = longueursilence % (2**nbitcodage)
-            message = message + np.binary_repr(valeur,2**nbitcodage)
-            j = j + longueursilence
-        else :
-            j=j+1
-    return message[30:30 + longueurmessage]
-        
+            i += 1
+    longueur_message = int(message, 2) * 8
+    while i < n and len(message) < (bits_nb_octets + longueur_message):
+        if est_silence(t, i):
+            longueur_silence = duree_silence(t, i)
+            valeur = longueur_silence % (2**nbitcodage)
+            message += np.binary_repr(valeur, nbitcodage)
+            i += longueur_silence
+        else:
+            i += 1
+    return message[bits_nb_octets:]
+
 #a=np.array([4,8,10,58,28000,-4561]) a.dtype -> dtype('int32')
 #a.astype('int16')
 #array([    4,     8,    10,    58, 28000, -4561], dtype=int16)
